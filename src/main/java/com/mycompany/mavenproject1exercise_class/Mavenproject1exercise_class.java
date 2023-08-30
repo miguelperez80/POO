7b464c99bf197d3591e2223596b9bfb352071865
miguelperez80 <142424602+miguelperez80@users.noreply.github.com>
2023-08-30 10:54:44 -0500

    Add files via upload

---
+++ src/main/java/com/mycompany/mavenproject1exercise_class/Mavenproject1exercise_class.java
@@ -0,0 +1,11 @@
+
+
+package com.mycompany.mavenproject1exercise_class;
+
+
+public class Mavenproject1exercise_class {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
