647a09bb5fee9c908cef21306dc7c3dbfd057e1f
miguelperez80 <142424602+miguelperez80@users.noreply.github.com>
2023-10-04 11:04:24 -0500

    Add files via upload

--- src/parcialpoo1/Parcialpoo1.java
+++ src/parcialpoo1/Parcialpoo1.java
@@ -6,11 +6,15 @@ public class Parcialpoo1 {
 
    
     public static void main(String[] args) {
-   Empleado empleado=new Empleado();
+   Empleado empleado=new Empleado("juan",3000);
+   Empleado_Asalariado salario1=new Empleado_Asalariado()
+   Empleado empleado1=new Empleado("pedro",3000);
+   Empleado empleado2=new Empleado("pablo",3000);
+   Empleado empleado3=new Empleado("jwdap",3000);
+   Empleado empleado4=new Empleado("mano",3000);
    
-    int empleados[];
+   
     
-    int empleados[0]= empleado1
                 
         
     }
