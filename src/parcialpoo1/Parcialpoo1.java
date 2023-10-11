c3458ce5d27b656e97da2f5b9464d06b195556ae
miguelperez80 <142424602+miguelperez80@users.noreply.github.com>
2023-10-11 09:48:07 -0500

    Update Parcialpoo1.java

--- src/parcialpoo1/Parcialpoo1.java
+++ src/parcialpoo1/Parcialpoo1.java
@@ -1,22 +1,67 @@
 
 package parcialpoo1;
-
+import java.util.ArrayList;
 
 public class Parcialpoo1 {
 
    
     public static void main(String[] args) {
-   Empleado empleado=new Empleado("juan",3000);
-   Empleado_Asalariado salario1=new Empleado_Asalariado()
-   Empleado empleado1=new Empleado("pedro",3000);
-   Empleado empleado2=new Empleado("pablo",3000);
-   Empleado empleado3=new Empleado("jwdap",3000);
-   Empleado empleado4=new Empleado("mano",3000);
-   
-   
-    
-                
-        
+   ArrayList<Empleado> empleados = new ArrayList<>();
+
+        // Llena el arreglo con al menos 10 datos diferentes
+        empleados.add(new Empleado_Asalariado("juan", 3000000, 500));
+        empleados.add(new Empleado_Asalariado("pedro", 3500000, 600));
+        empleados.add(new Empleado_PorHoras("marta", 0, 160, 60000));
+        empleados.add(new Empleado_PorHoras("carlos", 0, 180, 50000));
+        empleados.add(new Empleado_Asalariado("gavi", 2800000, 400));
+        empleados.add(new Empleado_Asalariado("leo", 3200000, 550));
+        empleados.add(new Empleado_PorHoras("jose", 0, 140, 50000));
+        empleados.add(new Empleado_PorHoras("miguel", 0, 200, 35000));
+        empleados.add(new Empleado_Asalariado("angel", 2700000, 450));
+        empleados.add(new Empleado_Asalariado("leider", 3100000, 520));
+
+        // Obtener Empleados por horas
+        System.out.println("Empleados por horas:");
+        for (Empleado empleado : empleados) {
+            if (empleado instanceof Empleado_PorHoras) {
+                System.out.println(empleado.getNombre() + " - Salario: $" + empleado.calcularSalario());
+            }
+        }
+
+        // Obtener Empleados Asalariados
+        System.out.println("\nEmpleados asalariados:");
+        for (Empleado empleado : empleados) {
+            if (empleado instanceof Empleado_Asalariado) {
+                System.out.println(empleado.getNombre() + " - Salario: $" + empleado.calcularSalario());
+            }
+        }
+
+        // Modificar el nombre o salario de un empleado 
+        for (Empleado empleado : empleados) {
+            if (empleado.getNombre().equals("juan")) {
+                empleado.setNombre("juan Modificado");
+                empleado.setSalarioBase(2000000);
+            }
+        }
+
+        // Obtener Empleados por horas nuevamente (para verificar que se modificó)
+        System.out.println("\nEmpleados por horas despues de la modificacion:");
+        for (Empleado empleado : empleados) {
+            if (empleado instanceof Empleado_PorHoras) {
+                System.out.println(empleado.getNombre() + " - Salario: $" + empleado.calcularSalario());
+            }
+        }
+
+        // Obtener un empleado específico 
+       
+        String empleadoBuscado = "jose";
+        for (Empleado empleado : empleados) {
+            if (empleado.getNombre().equals(empleadoBuscado)) {
+                System.out.println("\nEmpleado especifico (" + empleadoBuscado + "):");
+                System.out.println("Nombre: " + empleado.getNombre());
+                System.out.println("Salario: $" + empleado.calcularSalario());
+                break;
+            }
+        }
     }
-    
 }
