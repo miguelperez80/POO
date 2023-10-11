ddf9ffb8c0998e02ae4bee3d73643f4734b801c1
miguelperez80 <142424602+miguelperez80@users.noreply.github.com>
2023-10-11 10:15:20 -0500

    Update Parcialuno.java

--- src/parcialpoo1/Parcialuno.java
+++ src/parcialpoo1/Parcialuno.java
@@ -1,29 +1,33 @@
 
-package parcialpoo1;
+package parcialuno;
+
 import java.util.ArrayList;
 
-public class Parcialpoo1 {
 
-   
+public class Parcialuno {
+    
+
+  
     public static void main(String[] args) {
-   ArrayList<Empleado> empleados = new ArrayList<>();
+   
+        ArrayList<Empleado> empleados = new ArrayList<>();
 
         // Llena el arreglo con al menos 10 datos diferentes
-        empleados.add(new Empleado_Asalariado("juan", 3000000, 500));
-        empleados.add(new Empleado_Asalariado("pedro", 3500000, 600));
-        empleados.add(new Empleado_PorHoras("marta", 0, 160, 60000));
-        empleados.add(new Empleado_PorHoras("carlos", 0, 180, 50000));
-        empleados.add(new Empleado_Asalariado("gavi", 2800000, 400));
-        empleados.add(new Empleado_Asalariado("leo", 3200000, 550));
-        empleados.add(new Empleado_PorHoras("jose", 0, 140, 50000));
-        empleados.add(new Empleado_PorHoras("miguel", 0, 200, 35000));
-        empleados.add(new Empleado_Asalariado("angel", 2700000, 450));
-        empleados.add(new Empleado_Asalariado("leider", 3100000, 520));
+        empleados.add(new Asalariado("juan", 3000000, 500000));
+        empleados.add(new Asalariado("pedro", 3500000, 600000));
+        empleados.add(new EmpleadoPorHoras("marta", 0, 160, 60000));
+        empleados.add(new EmpleadoPorHoras("carlos", 0, 180, 50000));
+        empleados.add(new Asalariado("gavi", 2800000, 400000));
+        empleados.add(new Asalariado("leo", 3200000, 550000));
+        empleados.add(new EmpleadoPorHoras("jose", 0, 140, 50000));
+        empleados.add(new EmpleadoPorHoras("miguel", 0, 200, 35000));
+        empleados.add(new Asalariado("angel", 2700000, 450000));
+        empleados.add(new Asalariado("leider", 3100000, 520000));
 
         // Obtener Empleados por horas
         System.out.println("Empleados por horas:");
         for (Empleado empleado : empleados) {
-            if (empleado instanceof Empleado_PorHoras) {
+            if (empleado instanceof EmpleadoPorHoras) {
                 System.out.println(empleado.getNombre() + " - Salario: $" + empleado.calcularSalario());
             }
         }
@@ -31,32 +35,29 @@ public class Parcialpoo1 {
         // Obtener Empleados Asalariados
         System.out.println("\nEmpleados asalariados:");
         for (Empleado empleado : empleados) {
-            if (empleado instanceof Empleado_Asalariado) {
+            if (empleado instanceof Asalariado) {
                 System.out.println(empleado.getNombre() + " - Salario: $" + empleado.calcularSalario());
             }
         }
 
-        // Modificar el nombre o salario de un empleado 
+        // Modificar el nombre o salario de un empleado (por ejemplo)
         for (Empleado empleado : empleados) {
-            if (empleado.getNombre().equals("marta")) {
+            if (empleado.getNombre().equals("juan")) {
                 empleado.setNombre("juan Modificado");
-                empleado.setSalarioBase(1000000);
+                empleado.setSalarioBase(2000000);
             }
         }
 
         // Obtener Empleados por horas nuevamente (para verificar que se modificó)
         System.out.println("\nEmpleados por horas despues de la modificacion:");
         for (Empleado empleado : empleados) {
-            if (empleado instanceof Empleado_PorHoras) {
+            if (empleado instanceof EmpleadoPorHoras) {
                 System.out.println(empleado.getNombre() + " - Salario: $" + empleado.calcularSalario());
             }
         }
-        
 
-        // Obtener un empleado específico 
-       
-        //ejemplo 
-        String empleadoBuscado = "marta";
+        // Obtener un empleado específico (por ejemplo, "Empleado7")
+        String empleadoBuscado = "jose";
         for (Empleado empleado : empleados) {
             if (empleado.getNombre().equals(empleadoBuscado)) {
                 System.out.println("\nEmpleado especifico (" + empleadoBuscado + "):");
@@ -65,5 +66,8 @@ public class Parcialpoo1 {
                 break;
             }
         }
+    }
+}
+        
     }
 }
